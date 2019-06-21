9b2e0c0635f620902d11687e3e6c327ee1136b8b
James Craig <james.craig@gmail.com>
2019-06-21 12:56:18 -0400

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './site/**/*.html',
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-preset-env')({
+      browsers: "last 2 versions"
+    }),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ]
+};
